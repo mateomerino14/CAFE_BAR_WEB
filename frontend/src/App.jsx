d5882e13df6159fc11dfc0e72991926aa4ffd471
MAT988 <mateomerino988@gmail.com>
2026-08-05 18:14:59 -0400

    chore(frontend): setup inicial de Vite + React + Tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,7 @@
+import { AppRouter } from './router/AppRouter'
+
 function App() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-slate-900 text-white">
-      <h1 className="text-3xl font-bold underline">
-        ¡Tailwind funcionando en Café Bar!
-      </h1>
-    </div>
-  )
+  return <AppRouter />
 }
 
-export default App;
+export default App
